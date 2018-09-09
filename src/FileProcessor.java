6a61dc023be52192f2aa4d2b0ab3585b23ee9824
Saf <Saf@10.12.16.195>
2018-09-08 20:03:17 -0500

    Fixing eclipse

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -10,30 +10,27 @@ public class FileProcessor implements Runnable {
 	
 	public void run() {
 		BufferedReader fileReader;
-		String fileName = CSCI3850p0.getNextFile();
+		String fileName = "";
 		
 		try {
-			fileReader = new BufferedReader(new FileReader("./documentset/" + fileName));	
-			String str = "";
-			
-			while( (str = fileReader.readLine()) != null ) {
+			//do{
+				fileName = CSCI3850p0.getNextFile();
+				fileReader = new BufferedReader(new FileReader("./documentset/" + fileName));	
+				String str = "";
 				
-				str = str.replaceAll("<.*?>", "");
-				str = str.replaceAll("[^a-zA-Z0-9]", " ");
-				str = str.replaceAll("\\s+", " ");
-				str = str.toLowerCase();
+				while( (str = fileReader.readLine()) != null ) {
+					process(str, fileName);
+				}
 				
-				String tokens[] = str.split("\\s");
-				
-//				for( String token : tokens ) {
-//					
-//					if(!token.isEmpty()) {
-//						tokenQueue.add(token);
-//					}
+//				for( String key : tokenQueue )
+//				{
+//					System.out.println(key);
 //				}
-			}
-			
-			fileReader.close();
+				
+				output();
+				fileReader.close();
+				
+			//}while( !CSCI3850p0.isEmpty() );
 		} catch (FileNotFoundException e) {
 			System.out.println("Failed to open file: " + fileName);
 			e.printStackTrace();
@@ -46,17 +43,48 @@ public class FileProcessor implements Runnable {
 
 	}
 	
-	public void lineLinker(String toBreak, String docName){
-        	String[] arrs = toBreak.split(" ");
-        	for(int x = 0; x < arrs.length; x++){
-        		Node a = new Node();
-        		FileNode b = new FileNode();
-        		a.setKeyword(arrs[x]);
-        		a.setOccurance(1);
-        		b.setFileID(docName);
-        		b.setOccurrence(1);
-        		a.enQueue(b);
-        		tokenQueue.add(a);
-        	}
-    	}
+	public void process(String str, String fileName) {
+		str = str.replaceAll("<.*?>", "");
+		str = str.replaceAll("[^a-zA-Z0-9]", " ");
+		str = str.replaceAll("\\s+", " ");
+		str = str.toLowerCase();
+		
+		String tokens[] = str.split("\\s");
+		
+		for( String token : tokens ) {
+			
+			if(!token.isEmpty()) {
+				Node n = new Node();
+				n.setKeyword(token);
+				n.setOccurance(1);
+				
+				FileNode fn = new FileNode();
+				fn.setFileID(fileName);
+				fn.setOccurrence(1);
+				n.enQueue(fn);
+				
+				tokenQueue.add(n);
+				System.out.println(n.getKeyword());
+				//tokenQueue.add(token);
+			}
+		}
+	}
+	
+	public  void output() {
+		Dictionary.input( tokenQueue );
+	}
+	
+//	public void lineLinker(String toBreak, String docName){
+//        	String[] arrs = toBreak.split(" ");
+//        	for(int x = 0; x < arrs.length; x++){
+//        		Node a = new Node();
+//        		FileNode b = new FileNode();
+//        		a.setKeyword(arrs[x]);
+//        		a.setOccurance(1);
+//        		b.setFileID(docName);
+//        		b.setOccurrence(1);
+//        		a.enQueue(b);
+//        		tokenQueue.add(a);
+//        	}
+//    	}
 }
--- src/Node.java
+++ src/Node.java
@@ -7,14 +7,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class Node {
     private String keyword;
-    private int occurrence;
-    private ConcurrentLinkedQueue<FileNode> specific;
-    
-    public Node() {
-    	keyword = "";
-    	occurrence = 0;
-    	specific = new ConcurrentLinkedQueue<FileNode>();
-    }
+    private int occurrence = 0;
+    private ConcurrentLinkedQueue<FileNode> specific = new ConcurrentLinkedQueue<FileNode>();
     
     public String getKeyword() { return keyword; }    	
     public int getOccurance() { return occurrence; }
@@ -22,6 +16,30 @@ public class Node {
     public void setKeyword(String key) { keyword = key; }
     public void setOccurance(int oc) { occurrence = oc; }
     
+    public ConcurrentLinkedQueue<FileNode> getQueue() { return specific; }
+    public void setQueue( ConcurrentLinkedQueue<FileNode> queue ) { specific = queue; }
+    
     public FileNode deQueue() { return specific.remove(); }
     public void enQueue(FileNode node) { specific.add(node); }
+    
+    public void increment() {
+    	occurrence++;
+    }
+    
+    public boolean equals(Object e) {
+    	if (e == this)
+    		return true;
+    	
+    	if( !(e instanceof Node)) {
+    		return false;
+    	}
+    	
+    	Node n = (Node)e;
+    	
+    	return this.getKeyword().equals(n.getKeyword());
+    }
+    
+    public int compareTo( Node n ) {
+    	return this.getKeyword().compareTo( n.getKeyword() );
+    }
 }
