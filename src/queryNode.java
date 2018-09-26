6644f9a13353fa1c45b4e0ba1ce559907a0986f6
zalf <zalf@10.14.75.6>
2018-09-26 14:38:35 -0500

    bit of updates, seems to work

--- src/queryNode.java
+++ src/queryNode.java
@@ -1,7 +1,7 @@
 
 public class queryNode {
 	String docID;
-	float weight;
+	float weight = 0;
 	
 	public String getDocID() {return docID;}
 	public float getWeight() {return weight;}
@@ -9,4 +9,17 @@ public class queryNode {
 	public void setDocID(String a) {docID = a;}
 	public void setWeight(float b) {weight = b;}
 	public void addWeight(float c) {weight += c;}
+	
+	public boolean equals(Object e) {
+		if (e == this)
+    		return true;
+    	
+    	if( !(e instanceof Node)) {
+    		return false;
+    	}
+    	
+    	queryNode n = (queryNode)e;
+    	
+    	return this.getDocID().equals(n.getDocID());
+	}
 }
